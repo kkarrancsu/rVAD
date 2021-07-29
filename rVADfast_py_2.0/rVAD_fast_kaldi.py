a8d94cbb58f4a6c9078abe341c50bdc1f682b644
Kiran Karra <kiran.karra@jhuapl.edu>
2021-07-29 19:04:22 -0400

    bug fixes

--- rVADfast_py_2.0/rVAD_fast_kaldi.py
+++ rVADfast_py_2.0/rVAD_fast_kaldi.py
@@ -11,6 +11,9 @@ from copy import deepcopy
 
 import argparse
 import kaldi_io
+import subprocess
+from tqdm.auto import tqdm
+import numpy as np
 
 # Refs:
 #  [1] Z.-H. Tan, A.k. Sarkara and N. Dehak, "rVAD: an unsupervised segment-based robust voice activity detection method," Computer Speech and Language, 2019. 
@@ -19,10 +22,23 @@ import kaldi_io
 
 # 2017-12-02, Achintya Kumar Sarkar and Zheng-Hua Tan
 
+def count_file_lines(file_path):
+    """
+    Counts the number of lines in a file using wc utility.
+    :param file_path: path to file
+    :return: int, no of lines
+    """
+    num = subprocess.check_output(['wc', '-l', file_path])
+    num = num.split()
+    return int(num[0])
 
 def process_scp(wav_fp, vad_ark, vad_scp):
 
-    vad_output = 'ark:| copy-feats --compress=true ark:- ark,scp:%s,%s' % (vad_ark, vad_scp)
+    # TODO: check that copy-feats is on the path
+
+    nl = count_file_lines(wav_fp)
+    pbar = tqdm(total=nl)
+    vad_output = 'ark:| copy-vector ark:- ark,scp:%s,%s' % (vad_ark, vad_scp)
 
     with kaldi_io.open_or_fd(vad_output, 'wb') as f_out:
         with open(wav_fp, 'r') as f_in:
@@ -35,10 +51,12 @@ def process_scp(wav_fp, vad_ark, vad_scp):
                 f_id = l_split[0]
                 f_path = l_split[1]
 
-                compute_rVad(f_path, f_id, f_out)
+                vad = compute_rVad(f_path, f_id)
+                kaldi_io.write_vec_flt(f_out, vad.astype(np.float32), key=f_id)
+                pbar.update(1)
 
 
-def compute_rVad(fin_wav, fin_key, output_fobj):
+def compute_rVad(fin_wav, fin_key):
     winlen, ovrlen, pre_coef, nfilter, nftt = 0.025, 0.01, 0.97, 20, 512
     ftThres=0.5; vadThres=0.4
     opts=1
@@ -70,11 +88,8 @@ def compute_rVad(fin_wav, fin_key, output_fobj):
         fdata[range(int(noise_samp[j,0]),  int(noise_samp[j,1]) +1)] = 0
 
     vad_seg=speechproc.snre_vad(fdata,  nfr10, flen, fsh10, ENERGYFLOOR, pv01, pvblk, vadThres)
-
-    # numpy.savetxt(fvad, vad_seg.astype(int),  fmt='%i')
-    # print("%s --> %s " %(finwav, fvad))
-    kaldi_io.write_vec_flt(output_fobj, vad_seg, key=fin_key)
-
+    return vad_seg
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -85,3 +100,5 @@ if __name__ == '__main__':
     parser.add_argument('vad_scp', type=str,
                         help='Output SCP file to store vad results')  # output dir
     args = parser.parse_args()
+    process_scp(args.wav_scp, args.vad_ark, args.vad_scp)
+
